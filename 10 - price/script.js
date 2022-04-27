85facebacaffea1a4e5842c952815daf267cc306
Shuliak Liubov <shuliak.lyubov@gmail.com>
2022-04-27 15:10:47 +0300

    price

--- 10 - price/script.js
+++ 10 - price/script.js
@@ -1,4 +1,4 @@
-const progressGrowth = document.querySelector(".growth");
+const progressGrowth = document.querySelector(".pp_growth");
 
 progressGrowth.addEventListener("input", function () {
   const value = this.value === 0 ? 0 : this.value / 1000;
@@ -8,7 +8,7 @@ progressGrowth.addEventListener("input", function () {
     this.value + " subscribers";
 });
 
-const progress = document.querySelector(".professional");
+const progress = document.querySelector(".pp_professional");
 
 progress.addEventListener("input", function () {
   const value = this.value === 0 ? 0 : this.value / 5000;
@@ -16,3 +16,28 @@ progress.addEventListener("input", function () {
   document.querySelector(".subscribers_quantity_professional").innerHTML =
     this.value + " subscribers";
 });
+
+// const tbody = document.querySelectorAll("tbody");
+
+// const tr = document.createElement("tr");
+
+// if (window.matchMedia("(max-width: 1024px)").matches) {
+//   tbody.forEach((el) => {
+//     const td = el.children;
+
+//     [...td].forEach((el) => {
+//       const parent = el.parentNode;
+//       const newTr = document.createElement("tr");
+
+//       newTr.style.backgroundColor = el.style.background;
+//       const newTd = document.createElement("td");
+//       newTd.style.textAlign = "center";
+//       newTd.innerText = el.children[0].innerText;
+
+//       el.removeChild(el.children[0]);
+//       newTd.colSpan = 4;
+//       newTr.appendChild(newTd);
+//       parent.insertBefore(newTr, el);
+//     });
+//   });
+// }
