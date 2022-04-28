c86149110eded9b46f62a95f1d1e306c61ddc76f
Shuliak Liubov <shuliak.lyubov@gmail.com>
2022-04-28 13:59:19 +0300

    mobile menu

---
+++ style.js
@@ -0,0 +1,10 @@
+function extend() {
+  const gamburger = document.querySelector(".gamburger");
+  const mobileMenu = document.querySelector(".dp_menu_extend");
+  if (gamburger) {
+    gamburger.addEventListener("click", () => {
+      mobileMenu.classList.toggle("not_visible");
+    });
+  }
+}
+extend();
